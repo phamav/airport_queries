aa404cadeae122241a484fd6df351ab740434478
phamav <phamav@miamioh.edu>
2022-05-06 00:23:24 -0400

    commit

--- functions.cpp
+++ functions.cpp
@@ -27,7 +27,7 @@ void menu1(mysqlpp::Connection myDB) {
     std::cout << "Enter the city name in Germany >>" << std::endl;
     std::cin >> city;
 
-    query << "select * from Airports where City = " << city << " LIMIT 5;";
+    query << "SELECT * FROM Airports WHERE City = " << city << " LIMIT 5;";
 
     query.parse();  // check to ensure query is correct
     // Run the query and get stored results
@@ -49,7 +49,41 @@ void menu1(mysqlpp::Connection myDB) {
  *
  */
 void menu2(mysqlpp::Connection myDB) {
+    // Create a query
+    mysqlpp::Query query = myDB.query();
 
+    // Print the examples of cities.
+    std::cout << "Here are sample city names in Germany.  <---- possible input list (used limit 5)" << std::endl;
+    std::cout << "- Bautzen" << std::endl;
+    std::cout << "- Altenburg" << std::endl;
+    std::cout << "- Dessau" << std::endl;
+    std::cout << "- Eisenhuettenstadt" << std::endl;
+    std::cout << "- Suhl" << std::endl;
+
+    // Ask for user input (2 entries)
+    std::string city;
+    std::cout << "Enter the city name in Germany >>" << std::endl;
+    std::cin >> city;
+
+    int ID;
+    std::cout << "Enter the ID range >>" << std::endl;
+    std::cin >> ID;
+    query << "SELECT * FROM Airports WHERE City = " 
+    << city << " & ID <= " << ID << " LIMIT 5;";
+
+    query.parse();  // check to ensure query is correct
+    // Run the query and get stored results
+    mysqlpp::StoreQueryResult result = query.store();
+
+    std::cout << "Here are the airports in " << city << std::endl;
+    // Results is a 2D vector of mysqlpp::String objects.
+    // Print the results.
+    for (const auto& row : result) {
+        for (const auto& col : row) {
+            std::cout << col << "\t";
+        }
+        std::cout << std::endl;
+    }
 }
 
 void printMenu() {
